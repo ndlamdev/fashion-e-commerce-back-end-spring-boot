faf4e93f5a60fe7a424a06baa3dff7ed704ef1b1
Nguyễn Đình Lam <ndlam.dev@gmail.com>
2025-04-04 21:25:07 +0700

    feat: Cấu hình api gateway trỏ đến Authentication service

--- api_gateway_service/src/main/java/com/lamnguyen/api_gateway_service/config/RouterConfig.java
+++ api_gateway_service/src/main/java/com/lamnguyen/api_gateway_service/config/RouterConfig.java
@@ -8,17 +8,26 @@
 
 package com.lamnguyen.api_gateway_service.config;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class RouterConfig {
+	@Value("${server.servlet.context-path}")
+	String contextPath;
 
 	@Bean
-	public RouteLocator routeLocator(RouteLocatorBuilder builder){
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
+				.route("authentication-service", pre -> pre.path(contextPath + "/auth/**")
+						.filters(gatewayFilterSpec -> gatewayFilterSpec.stripPrefix(2))
+						.uri("lb://FASHION-E-COMMERCE-AUTHENTICATION-SERVICE"))
 				.build();
 	}
 }
