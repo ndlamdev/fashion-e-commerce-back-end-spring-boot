f00d12f6e813e59440c142501eecd366b9317b63
Nguyễn Đình Lam <ndlam.dev@gmail.com>
2025-04-07 11:34:21 +0700

    fix: Lỗi xung đột code khi merge

--- authentication_service/src/main/java/com/lamnguyen/authentication_service/model/Permission.java
+++ authentication_service/src/main/java/com/lamnguyen/authentication_service/model/Permission.java
@@ -17,15 +17,9 @@ import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
 
 import java.util.List;
-import java.util.Objects;
 
 @Entity
-<<<<<<<< HEAD:authentication_service/src/main/java/com/lamnguyen/authentication_service/model/Permission.java
 @Table(name = "permissions", uniqueConstraints = @UniqueConstraint(columnNames = {"name"}))
-========
-@Table(name = "roles", uniqueConstraints =
-@UniqueConstraint(columnNames = {"name"}))
->>>>>>>> dev:src/main/java/com/lamnguyen/fashion_e_commerce/model/Role.java
 @SuperBuilder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @RequiredArgsConstructor
@@ -36,21 +30,6 @@ public class Permission extends BaseEntity {
 	@Column(name = "`describe`")
 	String describe;
 
-<<<<<<<< HEAD:authentication_service/src/main/java/com/lamnguyen/authentication_service/model/Permission.java
 	@OneToMany(mappedBy = "permission", cascade = CascadeType.ALL)
 	List<PermissionOfRole> roles;
-========
-    @ManyToMany(mappedBy = "roles", fetch = FetchType.EAGER)
-    List<Permission> permissions;
-
-    @OneToMany(mappedBy = "role")
-    List<RoleOfUser> user;
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Role role = (Role) o;
-        return Objects.equals(id, role.id);
-    }
->>>>>>>> dev:src/main/java/com/lamnguyen/fashion_e_commerce/model/Role.java
 }
