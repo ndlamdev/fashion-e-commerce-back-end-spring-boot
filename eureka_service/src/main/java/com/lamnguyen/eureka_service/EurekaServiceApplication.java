c4ccf18dc4d57010fdcad9983b322a7b9fb1e34f
Nguyễn Đình Lam <ndlam.dev@gmail.com>
2025-04-29 08:22:52 +0700

    feat: Update security configuration and application properties for Eureka service

--- eureka_service/src/main/java/com/lamnguyen/eureka_service/EurekaServiceApplication.java
+++ eureka_service/src/main/java/com/lamnguyen/eureka_service/EurekaServiceApplication.java
@@ -3,6 +3,7 @@ package com.lamnguyen.eureka_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaServer
--- eureka_service/src/main/java/com/lamnguyen/eureka_service/config/SecurityConfiguration.java
+++ eureka_service/src/main/java/com/lamnguyen/eureka_service/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
-package com.lamnguyen.config_server.config;
+package com.lamnguyen.eureka_service.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,6 +10,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.util.Base64;
 
 @Configuration(proxyBeanMethods = false)
 public class SecurityConfiguration {
