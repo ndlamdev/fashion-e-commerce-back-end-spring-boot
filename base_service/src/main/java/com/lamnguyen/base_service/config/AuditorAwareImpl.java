65aabc72553d1cc04197765fdc0435d6773af77f
Nguyễn Đình Lam <ndlam.dev@gmail.com>
2025-05-02 17:05:30 +0700

    feat: create all basing feature in project

--- base_service/src/main/java/com/lamnguyen/base_service/config/AuditorAwareImpl.java
+++ base_service/src/main/java/com/lamnguyen/base_service/config/AuditorAwareImpl.java
@@ -2,10 +2,8 @@ package com.lamnguyen.base_service.config;
 
 import lombok.NonNull;
 import org.springframework.data.domain.AuditorAware;
-/*
 import org.springframework.data.mongodb.config.EnableMongoAuditing; // for mogodb
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing; // for jpa
-*/
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -13,8 +11,8 @@ import org.springframework.stereotype.Component;
 import java.util.Optional;
 
 @Component
-//@EnableMongoAuditing // for mogodb
-//@EnableJpaAuditing // for jpa
+@EnableMongoAuditing // for mogodb
+@EnableJpaAuditing // for jpa
 public class AuditorAwareImpl implements AuditorAware<String> {
 
 	@Override
