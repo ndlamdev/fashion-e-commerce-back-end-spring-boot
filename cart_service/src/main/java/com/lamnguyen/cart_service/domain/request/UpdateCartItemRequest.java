87168c9a8340c809861d34ebc92f20a6634c6982
Nguyễn Đình Lam <ndlam.dev@gmail.com>
2025-05-08 07:27:06 +0700

    feat: add custom validation annotations for quantity

--- cart_service/src/main/java/com/lamnguyen/cart_service/domain/request/UpdateCartItemRequest.java
+++ cart_service/src/main/java/com/lamnguyen/cart_service/domain/request/UpdateCartItemRequest.java
@@ -7,10 +7,12 @@
  **/
 package com.lamnguyen.cart_service.domain.request;
 
+import com.lamnguyen.cart_service.utils.annotation.NotEqualsIntegerNumber;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateCartItemRequest(
 		@NotNull
+		@NotEqualsIntegerNumber(message = "Quantity cannot be zero.")
 		Integer quantity
 ) {
 }
