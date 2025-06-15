10cb8ce9a50d51860f2555e3d33631072729e8dc
Nguyễn Đình Lam <ndlam.dev@gmail.com>
2025-06-15 07:27:07 +0000

    feat: Update Docker configurations and add Prometheus monitoring for services

--- docker/mongo/run.sh
+++ docker/mongo/run.sh
@@ -1,1 +1,2 @@
-docker run --name mongodb -p 27017:27017 -d mongodb/mongodb-community-server:latest
+docker run --name mongodb -v ./data/db:/data/db -v ./data/configdb:/data/configdb -p 27017:27017 -d mongodb/mongodb-community-server:latest
+
